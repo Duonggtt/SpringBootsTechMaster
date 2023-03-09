739cffcf925e76ba904a49b95634dc273f45ef09
Duonggtung <duonggtung173@gmail.com>
2023-03-09 09:07:06 +0700

    btvn

--- JsLearnTechMaster/js/main.js
+++ JsLearnTechMaster/js/main.js
@@ -1,60 +1,76 @@
-//1
+let products = [
+    {
+        name: "Iphone 13 Pro Max", // Tên sản phẩm
+        price: 3000000, // Giá mỗi sản phẩm
+        brand: "Apple", // Thương hiệu
+        count: 2, // Số lượng sản phẩm trong giỏ hàng
+    },
+    {
+        name: "Samsung Galaxy Z Fold3",
+        price: 41000000,
+        brand: "Samsung",
+        count: 1,
+    },
+    {
+        name: "IPhone 11",
+        price: 15500000,
+        brand: "Apple",
+        count: 1,
+    },
+    {
+        name: "OPPO Find X3 Pro",
+        price: 19500000,
+        brand: "OPPO",
+        count: 3,
+    },
+]
 
-let ans = 1;
-function calculateFactorial(num) {
-    for(let i = 1;i<= num;i++) {
-        ans *= i;
+//Thêm 1 sản phẩm bất kỳ vào trong mảng product
+function addNewProduct() {
+    products.push({
+        name: "IPhone 12",
+        price: 25500000,
+        brand: "Apple",
+        count: 2
+    });
+}
+addNewProduct();
+
+//Xóa tất cả sản phẩm của thương hiệu "Samsung" trong giỏ hàng
+
+function deleteProductByBrand() {
+    const newProduct = products.filter(item => item.brand !== "Samsung");
+    console.log(newProduct);
+}
+deleteProductByBrand();
+
+//Sắp xếp giỏ hàng theo price tăng dần
+
+function sortProductByPriceASC() {
+    products.sort((a,b) => a.price - b.price);
+    console.log(products);
+}
+sortProductByPriceASC();
+
+
+//Sắp xếp giỏ hàng theo count giảm dần
+
+function sortProductByCountDESC() {
+    products.sort((a,b) => b.count - a.count);
+    console.log(products);
+}
+sortProductByCountDESC();
+
+//Lấy ra 2 sản phẩm bất kỳ trong giỏ hàng
+
+function getRandomElement() {
+    const ans = [];
+    for(let i = 0 ; i < 2; i++) {
+        ans[i] = products[Math.floor(Math.random() * products.length)];
     }
     console.log(ans);
 }
+getRandomElement();
 
-calculateFactorial(5);
 
-//2
 
-function reverseString(str) {
-    let res = "";
-    for(let i = str.length - 1; i >= 0;i--) {
-        res += str[i];
-    }
-
-    console.log(res);
-}
-
-reverseString("hello")
-
-//3
-let country = "VN";
-
-switch(country) {
-    case "VN": 
-        console.log("Xin chao!");
-        break;
-    case "EN":
-        console.log("Hello!");
-        break;
-    case "CN":
-        console.log("Nihao!");
-        break;
-    case "TL":
-        console.log("KhapKhap!");
-        break;
-    default:
-        console.log("Invalid country");
-}
-
-//4
-
-function subString(str) {
-    if(str.length >= 15) {
-        let newStr = "";
-        for(let i = 0 ;i< 10;i++) {
-            newStr += str[i];
-        }
-        console.log(newStr + "...");
-    }else {
-        console.log("can nhap chuoi co do dai hon 15 ki tu!");
-    }
-}
-
-subString("helooooooooooooooooooooo");
