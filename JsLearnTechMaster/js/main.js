8952ee98939fb5fc105bd79b4942e92379db545c
Duonggtung <duonggtung173@gmail.com>
2023-03-13 11:39:12 +0700

    btvn

--- JsLearnTechMaster/js/main.js
+++ JsLearnTechMaster/js/main.js
@@ -1,76 +1,56 @@
-let products = [
-    {
-        name: "Iphone 13 Pro Max", // Tên sản phẩm
-        price: 3000000, // Giá mỗi sản phẩm
-        brand: "Apple", // Thương hiệu
-        count: 2, // Số lượng sản phẩm trong giỏ hàng
-    },
-    {
-        name: "Samsung Galaxy Z Fold3",
-        price: 41000000,
-        brand: "Samsung",
-        count: 1,
-    },
-    {
-        name: "IPhone 11",
-        price: 15500000,
-        brand: "Apple",
-        count: 1,
-    },
-    {
-        name: "OPPO Find X3 Pro",
-        price: 19500000,
-        brand: "OPPO",
-        count: 3,
-    },
-]
+//1
+const heading = document.getElementById('heading');
+heading.style.color = 'red';
+heading.style.textTransform = 'uppercase';
 
-//Thêm 1 sản phẩm bất kỳ vào trong mảng product
-function addNewProduct() {
-    products.push({
-        name: "IPhone 12",
-        price: 25500000,
-        brand: "Apple",
-        count: 2
-    });
-}
-addNewProduct();
+//2
 
-//Xóa tất cả sản phẩm của thương hiệu "Samsung" trong giỏ hàng
+const para = document.querySelectorAll('.para');
+Array.from(para).map(ele => ele.style.color = 'blue');
+Array.from(para).map(ele => ele.style.fontSize = '20px');
 
-function deleteProductByBrand() {
-    const newProduct = products.filter(item => item.brand !== "Samsung");
-    console.log(newProduct);
-}
-deleteProductByBrand();
+//3
 
-//Sắp xếp giỏ hàng theo price tăng dần
+const link = document.createElement('a');
+link.setAttribute("href","https://www.facebook.com/");
+link.innerHTML = "http://www.facebook.com";
 
-function sortProductByPriceASC() {
-    products.sort((a,b) => a.price - b.price);
-    console.log(products);
-}
-sortProductByPriceASC();
+const para3 = document.querySelector('.para-3'); 
+para3.insertAdjacentElement('afterend', link);
 
 
-//Sắp xếp giỏ hàng theo count giảm dần
+//4
 
-function sortProductByCountDESC() {
-    products.sort((a,b) => b.count - a.count);
-    console.log(products);
-}
-sortProductByCountDESC();
+let title = document.getElementById('title');
+title.innerText = "Đây là thẻ tiêu đề";
 
-//Lấy ra 2 sản phẩm bất kỳ trong giỏ hàng
+//5
 
-function getRandomElement() {
-    const ans = [];
-    for(let i = 0 ; i < 2; i++) {
-        ans[i] = products[Math.floor(Math.random() * products.length)];
-    }
-    console.log(ans);
-}
-getRandomElement();
+const desc = document.getElementsByClassName('description');
+desc[0].classList.add('text-bold');
+
+//6
+
+const btn = document.createElement('button');
+btn.innerText = "click me";
+para3.parentNode.replaceChild(btn, para3);
+
+//7
+
+const para2 = document.querySelector('.para-2');
+const newPara2 = para2.cloneNode(true);
+newPara2.innerText = para2.innerText;
+
+para2.insertAdjacentElement('beforebegin', newPara2);
+
+
+//8
+
+const para1 = document.querySelector('.para-1');
+para1.remove();
+
+
+
 
 
 
