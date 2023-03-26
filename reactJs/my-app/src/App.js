997628933441ef33b24f631f9c7b2a980e12fde0
Duonggtung <duonggtung173@gmail.com>
2023-03-26 22:45:15 +0700

    btvn

--- reactJs/my-app/src/App.js
+++ reactJs/my-app/src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import Counter from './components/Counter';
 import Ulli from './components/Ulli';
+import TotalBox from './components/TotalBox';
 
 function App() {
   return (
     <div className="App">
-      <Ulli/>
+      <TotalBox/>
     </div>
   );
 }
